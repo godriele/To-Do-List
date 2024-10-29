41fcf8f6e2c498f3767e8862489fd9cd8927e639
Godriele Dela Cruz <godriele11@yahoo.com>
2024-10-28 17:42:35 -0700

    Messed around with the twcss and made a mock design with fixing the input form design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./templates/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customGray: '#ede9e9',
+        customBeige: '#f5ebe0',
+        customLightBeige: '#d6ccc2',
+        customMuted: '#e3d5ca',
+        customDarkBeige: '#d5bdaf',
+    },
   },
+},
   plugins: [],
 }
